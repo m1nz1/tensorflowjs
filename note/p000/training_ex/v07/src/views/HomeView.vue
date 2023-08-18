97f692875ad25141a8eb077811f7a93c299a3265
zoselaslwl <smj980902@gmail.com>
2023-08-18 17:48:22 +0900

    202308158

---
+++ note/p000/training_ex/v07/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import USD from '@/components/USD.vue'
+// @ <- src 상대주소
+import JYP from '../components/JPY.vue'
+import { ref } from 'vue'
+const select = ref('USD')
+</script>
+<template>
+  <div>
+    <button @click="select = USD">달러</button>
+    <button @click="select = JYP">엔화</button>
+    <keep-alive>
+      <component :is="select"></component>
+    </keep-alive>
+  </div>
+</template>
+<script></script>
+<style></style>
